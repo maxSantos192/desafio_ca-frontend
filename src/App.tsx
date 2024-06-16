ce1699f6d1fc21be9001ec3bf9dd04b5d10a6036
maxsantos <maxsonsantos88@gmail.com>
2024-06-16 17:52:53 -0300

    feat: create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <h1 className="text-7xl">hello world</h1>;
-}
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/dashboard", element: <Dashboard /> },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+]);
+
+export default router;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Cidade Alta</h1>
+      <h2>Desafio Frontend</h2>
+    </div>
+  );
+};
+
+export default Header;
